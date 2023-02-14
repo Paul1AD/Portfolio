afce67ec9b0f5e6ad60acce1bcf783befa4129b7
Paul1AD <pawel.adam77@op.pl>
2023-02-14 15:00:50 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./style";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-primary w-full overflow-hidden">
+      {/*Header*/}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            Navbar
+          </div>
+        </div>
       </header>
+
+      {/*Main*/}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Main
+        </div>
+      </div>
+
+      {/*Section*/}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Setup
+        </div>
+      </div>
+
+
+      {/*Footer*/}
     </div>
   );
 }
